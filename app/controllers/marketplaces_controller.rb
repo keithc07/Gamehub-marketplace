f484fc366dae0d55965e6a5a455a5a881df226d8
Keith Ching <CAS022114@coderacademy.edu.au>
2021-11-11 15:53:55 +1100

    added controller

---
+++ app/controllers/marketplaces_controller.rb
@@ -0,0 +1,8 @@
+class MarketplacesController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+    
+end
---
+++ test/controllers/marketplaces_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class MarketplacesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
