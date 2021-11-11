d9aa4c096eff594b4d40ce570dfe5fdede1eaac6
Keith Ching <CAS022114@coderacademy.edu.au>
2021-11-11 23:14:14 +1100

    added validations to sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :timeoutable
+  validates :first_name, :last_name, :phone_number, :unit_address, :suburb, :postal_code, presence: true
 end
