b30aa1873776b8a3afb5cf4540186b10a05f9e35
Keith Ching <CAS022114@coderacademy.edu.au>
2021-11-14 12:04:51 +0900

    complete validations and added error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :timeoutable
-  validates :first_name, :last_name, :phone_number, :unit_address, :suburb, :postal_code, :state, presence: true
+
+  validates :phone_number, numericality: { only_integer: true, message: "only allows integers"}, length: { is: 9, message: "require 9 digits" }, uniqueness: true, presence: true
+  validates :first_name, :last_name, :suburb, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, presence: true
+  validates :unit_address, :postal_code, presence: true
+
+  validates :state, length: { in: 2..3 }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows 2-3 letters" }, presence: true
+
   has_many :listings
   has_many :buys
 end
