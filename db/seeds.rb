70e410420e21d5d3818ace91e909bfed9b70f792
Keith Ching <CAS022114@coderacademy.edu.au>
2021-11-18 14:06:02 +1100

    verify on database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Console.create( id: 1, console_type: 'PS4/PS5')
-Console.create( id: 2, console_type: 'XboxOne')
-Console.create( id: 3, console_type: 'Nintendo Switch')
-Console.create( id: 4, console_type: 'PC')
-Console.create( id: 5, console_type: 'Others')
-
 State.create( id: 1, state_name: 'NSW')
 State.create( id: 2, state_name: 'QLD')
 State.create( id: 3, state_name: 'SA')
 State.create( id: 4, state_name: 'TAS')
 State.create( id: 5, state_name: 'VIC')
 State.create( id: 6, state_name: 'WA')
+
+Console.create( id: 1, console_type: 'PS4/PS5')
+Console.create( id: 2, console_type: 'XboxOne')
+Console.create( id: 3, console_type: 'Nintendo Switch')
+Console.create( id: 4, console_type: 'PC')
+Console.create( id: 5, console_type: 'Others')
